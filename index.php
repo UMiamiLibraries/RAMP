3db01f593f010abe280c98a2d7186de81b637abf
James Little <j.little@miami.edu>
2013-09-05 15:53:51 -0400

    more visual changes

--- index.php
+++ index.php
@@ -2,17 +2,29 @@
 include('header.php');
 ?>
 
+<<<<<<< HEAD
+=======
 <h1>Welcome to RAMP</h1>
 
 
+>>>>>>> 164c5fbe7bf1c522ce308c5353389b9c6aad9b32
 <div class="pure-g-r">
 
   <div class="pure-u-1">
   <div class="content_box">
   <img src="style/images/convert.png" alt="Convert" width="24px" height="24px"/>
+<<<<<<< HEAD
+  <h1>Convert EAD Records Into EAC-CPF Records</h1>
+  
+
+ <button data-href="ead_convert.php" class="update_button pure-button pure-button-primary" id="convert_ead">Convert EAD Files</button>
+
+  
+=======
   <h1>Convert EAD Records Into EAC-CPF Records
   <p>
   </p>
+>>>>>>> 164c5fbe7bf1c522ce308c5353389b9c6aad9b32
   </div>
   </div>
 
@@ -37,7 +49,11 @@ $results = $mysqli->query ("SELECT ead_file, ExtractValue(eac_xml, '/descendant-
 
 
 echo  "<select id='ead_files'>";
+<<<<<<< HEAD
+echo "<option>Select a name</option>";
+=======
 echo "<option value=''></option>";
+>>>>>>> 164c5fbe7bf1c522ce308c5353389b9c6aad9b32
 while ($row = $results->fetch_assoc()) {
   $name = $row["Name"];
   $file_name = $row["ead_file"];
@@ -74,12 +90,20 @@ print ("</select>");
   <div class="content_box">
   <img src="style/images/new.png" alt="Convert" width="24px" height="24px"/>
   <h1>Create a New Skeleton EAC-CPF Record</h1>
+<<<<<<< HEAD
+ <p>
+ </p>
+  <select>
+<option>
+Create a new ...
+=======
   <p>
   Create a new 
   </p>
   <select>
 <option>
 
+>>>>>>> 164c5fbe7bf1c522ce308c5353389b9c6aad9b32
 </option>
   <option value='person'>
 Person
