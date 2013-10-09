2276f39bf4435062d9d5e4139a2eeda06f3ba2ba
Tim Thompson <timathom@gmail.com>
2013-10-09 11:54:55 -0400

    New record form update.

--- save_new.php
+++ save_new.php
@@ -1,241 +1,141 @@
 <?php
 /*
-
-  This script accepts a POST request containing the data that the user wants to appear in a a new EAC record. It takes that data and creates a stub EAD record. Then it inserts that into the database. On the front-end, an ajax request is made to the EAD to EAC conversion class. Thus, you end up with a new EAD and EAC record in the database.
+  This script displays a form for creating a new EAC record.
 
   -- Jamie
 
 */
 
-include('conf/db.php');
-include('conf/inst_info.php');
-include('conf/includes.php');
-include('classes/EadConvert.php');
-include('classes/Database.php');
-include('classes/XsltTransform.php');
-include('php-diff/lib/Diff.php');
-include('php-diff/lib/Diff/Renderer/Html/SideBySide.php');
-
-$ramp_id = 'RAMP-' . rand(0,10090294092942);
-
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_default, $db_port);
-if ($mysqli->connect_errno) {
-  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
-// Basic input validation
-$cleanedArray = array();
-
-foreach ($_POST as $post_name => $post_val) {
-    if ($post_name == "entity") {
-        checkCharacters($post_val);
-    }
-    if ($post_name == "name") {
-        checkCharacters($post_val);
-    }    
-    if ($post_name == "bioghist") {
-        $post_val = mysqli_real_escape_string($mysqli,$post_val);        
-        $post_val = strip_tags($post_val);
-        //$post_val = htmlentities($post_val);
-    }    
-    if ($post_name == "type") {
-        checkCharacters($post_val);
-    }                
-    $cleanedArray[$post_name] = $post_val;
-} 
-
-//make file name valid utf8
-$file_name = $cleanedArray["name"];
-$file_name_lower = strtolower(str_replace(' ', '_', $file_name));
-$file_name_lower = preg_replace('/[^a-zA-Z0-9-_]/', '', $file_name_lower);
-$file_name_lower = iconv('utf-8', "us-ascii//TRANSLIT", $file_name_lower);
-$file_name_lower = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $file_name_lower);
-
-//exit and return message if file name is empty
-if( $file_name_lower == "" )
-	die( "Record not saved. File name is empty." );
-
-//assign values to cleaned array
-$entity = $cleanedArray["entity"];
-$entname = $cleanedArray["name"];
-$bioghist = $cleanedArray["bioghist"];
-$type = $cleanedArray["type"];
-
-//check for newlines in bioghist
-/*
-$lines = strpos($bioghist, '\n\n');
-
-if ($bioghist != "" && $lines === false) {
-    die( "Please enter a space between each paragraph of the biography/history." );    
-} 
-*/
-
-// instinfo
-if (file_exists(  $_POST["dir"] . '/' . $file_name_lower . '.xml')) {
-
-  echo "A record with this name already exists.";
-
-} else {  
-  touch(  $_POST["dir"] . '/' . $file_name_lower . '.xml');
-
-  $f = fopen(  $_POST["dir"] . '/' . $file_name_lower . '.xml', "w");
-
-  $ead_doc = new DOMDocument();
-  $ead_doc->formatOutput = true; 
-
-  switch(strtolower( $type )) {
-
-  case 'person':
-    try {
-      $ead_doc->loadXML('<ead audience="external"
-		     xmlns="urn:isbn:1-931666-22-9"
-		     xmlns:xlink="http://www.w3.org/1999/xlink"
-		     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		     xsi:schemaLocation="http://www.loc.gov/ead/ http://www.loc.gov/ead/ead.xsd">
-		   <eadheader langencoding="iso639-2b" audience="external" countryencoding="iso3166-1" dateencoding="iso8601" repositoryencoding="iso15511" scriptencoding="iso15924" relatedencoding="MARC21">
-		      <eadid encodinganalog="856$u" mainagencycode="' . $agency_code  . '" countrycode="US" identifier="' . $ramp_id . '"></eadid>
-		      <filedesc>
-		        <titlestmt>
-		               <titleproper encodinganalog="245"></titleproper>
-		                  </titlestmt>
-		      </filedesc>
-		</eadheader>
-		<frontmatter>
-		            </frontmatter>
-		            <archdesc audience="external" relatedencoding="MARC21">
-		               <did>
-		                     <origination label="Creator" encodinganalog="245$c">
-		                     <persname encodinganalog="100"  source="local">' . $entname .
+include('header.php');
+?>
+<table id="new_eac_table">
+<tr>
+<td>
+<form id="new_eac_form">
+  <br/>
+  <p class="note">Note: new record form is still in beta. Currently, only Entity Type, Name, and Biography have been enabled. Please monitor the <a href="https://github.com/UMiamiLibraries/RAMP">RAMP GitHub repository for updates</a>.</p> 
+  <label style="display:inline;">Entity Type</label>
+  <select id="entity_type">
+    <option>Person</option>
+    <option>Corporate Body</option>
+    <option>Family</option>
+  </select>
+  <br/>
+  <br/>
+  <label>Name and dates (format: Last Name, First Name, 9999-9999)</label>
+  <input id="eac_name" type="text" size="50"/>
+  <br/>
+  <br/>
+  <!--
+  <label>Dates of existence</label>
+  <label style="display:inline;">From</label>
+  <input id="from" type="text" disabled/>
+  <label style="display:inline;">To</label>
+  <input id="to" type="text" disabled/>
+  <br/>
+  <br/>
+  <label>Associated places <span style="font-style:italic;">(enter each place on a new line)</span></label>
+  <label>Places and associated dates must be preceded by <a href="http://www.loc.gov/marc/authority/ad370.html" target="_blank">MARC 370 subfield delimiters</a> ($a, $b, $c, $e, $f, $g, $s, $t).</label>
+  <textarea cols="60" style="height:80px;margin-left:0; margin-bottom:1%;" id="places" disabled></textarea>
+  <br/>
+  <br/>
+  <label>Associated group <span style="font-style:italic;">(enter each group on a new line)</span></label>
+  <label>Groups and associated dates must be preceded by <a href="http://www.loc.gov/marc/authority/ad373.html" target="_blank">MARC 373 subfield delimiters</a> ($a, $s, $t).</label>
+  <textarea cols="60" style="height:80px; margin-left:0; margin-bottom:1%;" id="group" disabled></textarea>
+  <br/>
+  <br/>
+  <label>Occupation or field of activity <span style="font-style:italic;">(enter each item on a new line)</span></label>
+  <label>Activities and associated dates must be preceded by <a href="http://www.loc.gov/marc/authority/ad374.html" target="_blank">MARC 374 subfield delimiters</a> ($a, $s, $t).</label>
+  <textarea cols="60" style="height:80px; margin-left:0; margin-bottom:1%;" id="activity" disabled></textarea>
+  <br/>
+  <br/>
+  <label>Gender</label>
+  <select id="gender" disabled>
+    <option></option>
+    <option>female</option>
+    <option>male</option>
+    <option>other</option>
+  </select>
+  <br/>
+  <br/>
+  <label>Language <span style="font-style:italic;">(enter each item on a new line)</span></label>
+  <label>Three-letter codes and language names must be preceded by <a href="http://www.loc.gov/marc/authority/ad377.html" target="_blank">MARC 377 subfield delimiters</a> ($a, $l).</label>
+  <textarea cols="60" style="height:80px; margin-left:0; margin-bottom:1%;" id="lang" disabled></textarea>  
+  <br/>
+  <br/>
+  -->
+  <label>Biography or history <span style="font-style:italic;">(enter a blank line between each paragraph)</span></label>
+  <textarea cols="60" style="margin-left:0; margin-bottom:1%; font-size:1em;" id="bioghist"></textarea>
+  <br/>
+  <br/>
+  <!--
+  <label>Sources <span style="font-style:italic;">(enter a blank line between each source)</span></label>
+  <textarea cols="60" style="height:80px; margin-left:0; margin-bottom:2%;" id="sources" disabled></textarea>
+  -->
+  <br/>
+  </form>
+  </td>
+  </tr>
+  <tr>
+  <td>
+      <button id="submit_new" class="pure-button pure-button-primary">Create</button>  
+  </td>
+  </tr>
+  </table>
+  
+  <p id="results" style="margin-left:7px;"></p>
+  <br/>
+  <br/>
 
 
-			'</persname>
-		                           </origination>
-		                           <note type="creation">Record created in RAMP.</note>
-		      </did>
-		               <bioghist encodinganalog="545">
-		                  <p>' . $bioghist . '</p>
-		               </bioghist>
 
+  <script>
 
-		   </archdesc>
-		</ead>
-		');
-      // Try to load the ead file
+   var $savedialog = $('<div></div>')
+      .html('Saved New Record')
+      .dialog({
+        autoOpen: false,
+        buttons: {
+          "OK" : function () {
+            $( this ).dialog( "close" );
+          }
+        }
+    });
 
-    } catch(Exception $e) {
-      die ('Caught exception: ' .  $e->getMessage() . "\n");
-    }
+  $('#submit_new').click(function() {
+      $('#results').html('');
 
-    break;
+      $.post("save_new.php", {
+        type: $('#entity_type').val(),
+	    entity: $('#entity_type').val(),
+	    name: $('#eac_name').val(),
+	    from: $('#from').val(),
+	    to: $('#to').val(),
+	    places: $('#places').val(),
+	    group: $('#group').val(),
+	    activity: $('#activity').val(),
+	    gender: $('#gender').val(),
+	    lang: $('#lang').val(),
+	    bioghist: $('#bioghist').val(),
+	    sources: $('#sources').val(),
+      	dir : <?php echo  '"' . addslashes($ead_path) . '"'; ?>
 
-  case 'corporate body':
-    try {
-      $ead_doc->loadXML('<ead audience="external"
-		     xmlns="urn:isbn:1-931666-22-9"
-		     xmlns:xlink="http://www.w3.org/1999/xlink"
-		     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		     xsi:schemaLocation="http://www.loc.gov/ead/ http://www.loc.gov/ead/ead.xsd">
-		   <eadheader langencoding="iso639-2b" audience="external" countryencoding="iso3166-1" dateencoding="iso8601" repositoryencoding="iso15511" scriptencoding="iso15924" relatedencoding="MARC21">
-		      <eadid encodinganalog="856$u" mainagencycode="' . $agency_code  . '" countrycode="US" identifier="' . $ramp_id . '"></eadid>
-		      <filedesc>
-		        <titlestmt>
-		               <titleproper encodinganalog="245"></titleproper>
-		                  </titlestmt>
-		      </filedesc>
-		</eadheader>
-		<frontmatter>
-		            </frontmatter>
-		            <archdesc audience="external" relatedencoding="MARC21">
-		               <did>
-		                     <origination label="Creator" encodinganalog="245$c">
-		                     <corpname encodinganalog="100"  source="local">' . $entname . '</corpname>
-		                           </origination>
-		                           <note type="creation">Record created in RAMP.</note>
-		      </did>
-		               <bioghist encodinganalog="545">
-		<p>' . $bioghist . '</p>
-		                           </bioghist>
-		   </archdesc>
-		</ead>
-		');
-    } catch(Exception $e) {
-      die ('Caught exception: ' .  $e->getMessage() . "\n");
-    }
+	    }, function (data) {
 
-    break;
+	   $savedialog.html(data).dialog('open');
 
-  case 'family':
+      window.setTimeout(slowreload, 1000);
 
-    try{
-      $ead_doc->loadXML('<ead audience="external"
-		     xmlns="urn:isbn:1-931666-22-9"
-		     xmlns:xlink="http://www.w3.org/1999/xlink"
-		     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		     xsi:schemaLocation="http://www.loc.gov/ead/ http://www.loc.gov/ead/ead.xsd">
-		   <eadheader langencoding="iso639-2b" audience="external" countryencoding="iso3166-1" dateencoding="iso8601" repositoryencoding="iso15511" scriptencoding="iso15924" relatedencoding="MARC21">
-		      <eadid encodinganalog="856$u" mainagencycode="' . $agency_code  . '" countrycode="US" identifier="' . $ramp_id . '"></eadid>
-		      <filedesc>
-		        <titlestmt>
-		               <titleproper encodinganalog="245"></titleproper>
-		                  </titlestmt>
-		      </filedesc>
-		</eadheader>
-		<frontmatter>
-		            </frontmatter>
-		            <archdesc audience="external" relatedencoding="MARC21">
-		               <did>
-		                     <origination label="Creator" encodinganalog="245$c">
-		                     <famname  source="local">' . $entname . '</famname>
-		                           </origination>
-		                           <note type="creation">Record created in RAMP.</note>
-		      </did>
-		               <bioghist encodinganalog="545">
-		<p>' . $bioghist .
-			'</p>
-		                           </bioghist>
+      function slowreload() {
+        location.reload();
+      }
 
+	});
 
-		   </archdesc>
-		</ead>
-		');
-    } catch(Exception $e) {
-      die ('Caught exception: ' .  $e->getMessage() . "\n");
-    }
+    });
 
-    break;
-  }
+</script>
 
-  fwrite($f, $ead_doc->saveXML());
-
-    $ead_convert = new EadConvert( $_POST["dir"] );
-	$ead_convert->setAgency_code($agency_code);
-	$ead_convert->setOther_agency_code($other_agency_code);
-	$ead_convert->setAgency_name($agency_name);
-	$ead_convert->setShortAgency_name($short_agency_name);
-	$ead_convert->setServer_name($serverName);
-	$ead_convert->setLocal_url($localURL);
-	$ead_convert->setRepository_one($repositoryOne);
-	$ead_convert->setRepository_two($repositoryTwo);
-	$ead_convert->setEventDescDerive($eventDescDerive);
-    $ead_convert->setEventDescRevise($eventDescRevise);
-	$ead_convert->setEventDescCreate($eventDescCreate);
-	$ead_convert->setEventDescExport($eventDescExport);
-	//$ead_convert->setEventDescRAMP($eventDescRAMP); // Removed because created Diff conflict. --timathom
-    $ead_convert->new_eac( $file_name_lower );
-
-  echo "Sucessfully created new record.";
-
-}
-
-// Function for basic validation of character input.
-function checkCharacters($string) {
-    $string = trim($string);
-    $string = stripslashes(strip_tags($string));
-    $string = htmlentities($string);
-    $string = mysqli_real_escape_string($mysqli,$string);    
-    //$found = preg_match("/^[a-zA-Z]$/", $string);		
-    //return $found;
-    return $string;
-}
-
+<?php
+include('footer.php');
 ?>
