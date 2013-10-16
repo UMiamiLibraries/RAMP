1fa01354aea69615cd6bc20e24dd384616aebdbe
Tim Thompson <timathom@gmail.com>
2013-10-15 21:34:11 -0400

    Added comment to inst_info parameter.

--- conf/inst_info.php
+++ conf/inst_info.php
@@ -14,10 +14,10 @@
 
 $agency_code = "ABC-D"; // "The code that represents the institution or service responsible for the creation, maintenance and/or dissemination of the EAC-CPF instance."
 $other_agency_code = "XYZ"; // "Alternate code representing the instituion or service responsible for the creation, maintenance, and/or dissemination of the EAC-CPF instance."
-$agency_name = "University of ABC"; // Name displayed in the RAMP footer.
+$agency_name = "University of ABC"; // Name displayed in the RAMP footer and in "agencyName" element.
 $short_agency_name = "ABC"; // Abbreviation of institution's name; for example, used in "otherRecordId" @localType for merged records.
 $serverName = "abc_server"; // Name of server where EAD finding aids are hosted (e.g., "proust" or "gryphon"). Helps filter out "ead:extref" data in ead2eac.xsl.
-$localURL = "http://abcuniversity.edu/?id="; // Base URL for local finding aids: i.e., whatever precedes the finding aid's unique ID in the URL string.
+$localURL = "http://hdl.loc.gov/"; // Base URL for local finding aids: i.e., whatever precedes the finding aid's unique ID in the URL string.
 $repositoryOne = "REPO1"; // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
 $repositoryTwo = "REPO2"; // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
 
