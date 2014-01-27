60872bcba5df35de6ef297278af8aed03dcfa84d
timathom <timathom@timathom-Ultra.(none)>
2014-01-27 18:31:39 -0500

    Clean up inst_info.php.

--- conf/inst_info.php
+++ conf/inst_info.php
@@ -7,24 +7,22 @@
    
    (1) "pDiscServ" stores a URL for a local discovery service (e.g., Summon) in order to add a link to the wiki markup in the "External links" section. 
    
-   (2) "pFindingAidInfo" stores publication info (like institution name, place, and date) for finding aid citations in the wiki markup "Notes and references" section. 
-
-   --timathom
+   (2) "pFindingAidInfo" stores publication info (like institution name, place, and date) for finding aid citations in the wiki markup "Notes and references" section.   
 */
 
-$agency_code = "ABC-D"; // "The code that represents the institution or service responsible for the creation, maintenance and/or dissemination of the EAC-CPF instance."
-$other_agency_code = "XYZ"; // "Alternate code representing the instituion or service responsible for the creation, maintenance, and/or dissemination of the EAC-CPF instance."
-$agency_name = "University of ABC"; // Name displayed in the RAMP footer and in "agencyName" element.
-$short_agency_name = "ABC"; // Abbreviation of institution's name; for example, used in "otherRecordId" @localType for merged records.
+$agency_code = "ABC-D";     // "The code that represents the institution or service responsible for the creation, maintenance and/or dissemination of the EAC-CPF instance."
+$other_agency_code = "XYZ"; // "Alternate code representing the institution or service responsible for the creation, maintenance, and/or dissemination of the EAC-CPF instance."                             
+$agency_name = "University of ABC"; // Name displayed in the RAMP footer and in "agencyName" element. 
+$short_agency_name = "ABC"; // Abbreviation of institution's name; for example, used in "otherRecordId" @localType for merged records. For example, "UML" for University of Miami Libraries.
 $serverName = "abc_server"; // Name of server where EAD finding aids are hosted (e.g., "proust" or "gryphon"). Helps filter out "ead:extref" data in ead2eac.xsl.
-$localURL = "http://"; // Base URL for local finding aids: i.e., whatever precedes the finding aid's unique ID in the URL string.
-$repositoryOne = "REPO1"; // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
-$repositoryTwo = "REPO2"; // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
+$localURL = "http://";      // Base URL for local finding aids: i.e., whatever precedes the finding aid's unique ID in the URL string.
+$repositoryOne = "REPO1";   // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
+$repositoryTwo = "REPO2";   // Abbreviation for local archival repository. Can be used to help filter out "ead:relatedmaterials" data in ead2eac.xsl.
 
-// Different possible "eventDescription" values that can be set in ead2eac.xsl.
+// Here are some different possible "eventDescription" values that can be set in ead2eac.xsl.
 $eventDescDerive = "New EAC-CPF record derived from EAD instance and existing EAC-CPF record."; 
 $eventDescCreate = "Record created.";
 $eventDescRevise = "Record revised.";
 $eventDescExport = "Record exported as EAC-CPF/XML.";
-//$eventDescRAMP = "Record created in RAMP."; // Removed because created Diff conflict. --timathom
+
 ?>
