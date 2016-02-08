190009d3e229b916f4702032c05d01884136a45f
James Little <j.little@miami.edu>
2016-02-08 09:04:10 -0500

    Adding composer.json and tests folder

---
+++ tests/_data/ramp.sql
@@ -0,0 +1,123 @@
+CREATE DATABASE  IF NOT EXISTS `ead_eac` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `ead_eac`;
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: s52298__ead_eac
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `eac`
+--
+
+DROP TABLE IF EXISTS `eac`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `eac` (
+  `eac_id` int(11) NOT NULL AUTO_INCREMENT,
+  `eac_xml` mediumtext,
+  `published` tinyint(1) DEFAULT '0',
+  `ead_file` varchar(200) DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`eac_id`),
+  UNIQUE KEY `eac_id` (`eac_id`),
+  UNIQUE KEY `ead_file` (`ead_file`),
+  KEY `fk_ead_file_idx` (`ead_file`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eac`
+--
+
+LOCK TABLES `eac` WRITE;
+/*!40000 ALTER TABLE `eac` DISABLE KEYS */;
+/*!40000 ALTER TABLE `eac` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ead`
+--
+
+DROP TABLE IF EXISTS `ead`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ead` (
+  `ead_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ead_xml` mediumtext,
+  `ead_file` varchar(200) NOT NULL,
+  PRIMARY KEY (`ead_id`),
+  UNIQUE KEY `ead_file` (`ead_file`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ead`
+--
+
+LOCK TABLES `ead` WRITE;
+/*!40000 ALTER TABLE `ead` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ead` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mediawiki`
+--
+
+DROP TABLE IF EXISTS `mediawiki`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mediawiki` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `wiki_text` mediumtext NOT NULL,
+  `updated` varchar(45) DEFAULT NULL,
+  `created` varchar(45) DEFAULT NULL,
+  `eac_id` int(11) NOT NULL,
+  `published` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `eac_id_UNIQUE` (`eac_id`),
+  KEY `fkeacid_idx` (`eac_id`),
+  CONSTRAINT `fkeacid` FOREIGN KEY (`eac_id`) REFERENCES `eac` (`eac_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mediawiki`
+--
+
+LOCK TABLES `mediawiki` WRITE;
+/*!40000 ALTER TABLE `mediawiki` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mediawiki` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `ead_eac`.`eac_BINS`
+BEFORE INSERT ON `ead_eac`.`mediawiki`
+FOR EACH ROW
+
+	SET NEW.created = NOW(), NEW.updated = NOW() */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
---
+++ tests/acceptance/HomeCept.php
@@ -0,0 +1,7 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('ensure that frontpage works');
+$I->amOnPage('/'); 
+$I->see('Welcome to RAMP');
+?>
+
