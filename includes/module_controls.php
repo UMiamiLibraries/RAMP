b65d330a212bd07b20f9b058ac1e52817a5b2499
cbrownroberts <charlesbrownroberts@miami.edu>
2016-02-18 12:19:30 -0500

    removed main_edit class from ingest buttons

--- includes/module_controls.php
+++ includes/module_controls.php
@@ -1,7 +1,7 @@
 <div id="module_controls">
-  <span id="ingest_buttons" class="main_edit">
-      <button id="ingest_worldcat" class="ingest_button pure-button pure-button-primary main_edit">WorldCat Identities</button>
-      <button id="ingest_viaf" class="ingest_button pure-button pure-button-primary main_edit">VIAF</button>
-      <button id="convert_to_wiki" class="pure-button pure-button-primary main_edit">Wikipedia</button>
+  <span id="ingest_buttons">
+      <button id="ingest_worldcat" class="ingest_button pure-button pure-button-primary">WorldCat Identities</button>
+      <button id="ingest_viaf" class="ingest_button pure-button pure-button-primary">VIAF</button>
+      <button id="convert_to_wiki" class="pure-button pure-button-primary">Wikipedia</button>
   </span>
 </div>
