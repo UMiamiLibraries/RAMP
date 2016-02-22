ccea161c6e0c0b6ae9e055903a311c0efa4166c2
agdarby <agdarby@gmail.com>
2016-02-22 14:09:21 -0500

    remove uml logo; maybe this could go (in full colour) on About page

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	<div class="inner-area">
 		<div class="pure-g">
 			<div class="pure-u-2-3">
-				<a href="http://library.miami.edu/" target="_blank"><img src="style/images/um-lib-logo.png" class="lib-logo" /></a>
+				
 			</div>
 			<div class="pure-u-1-3">
 				<p class="ramp-link"><a href="https://github.com/UMiamiLibraries/RAMP" target="_blank">RAMP GitHub Project</a> <i class="fa fa-github"></i></p>
