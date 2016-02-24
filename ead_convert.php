47c2b2f46eefb04686a948322ab4fd0818138a12
cbrownroberts <charlesbrownroberts@miami.edu>
2016-02-24 16:47:54 -0500

    added pure container

--- ead_convert.php
+++ ead_convert.php
@@ -63,41 +63,46 @@ include('header.php');
         }
     </script>
 
+    <div class="inner-area">
 
     <div class="pure-g">
+        <div class="pure-u-1-1">
+            <h1>Import or Upload EAD Files</h1>
+            <p id="convert_message">On this page you can convert EAD files or import EAC-CPF files that you have
+                placed in the 'ead' folder during the install process.</p>
+            <p> After importing you can export and download the records.</p>
+        </div>
         <div class="pure-u-1-2">
-            <h1>Upload from the EAD Folder</h1>
+            <strong>Upload an EAD</strong>
+            <form enctype="multipart/form-data" action="ajax/uploader.php" method="POST">
+                <input type="hidden" name="MAX_FILE_SIZE" value="30000000"/>
+                 <input name="ead" type="file"/><br>
+                <input type="submit" class="pure-button ramp-button" value="Upload EAD"/>
+            </form>
+        </div>
+
+        <div class="pure-u-1-2">
+            <strong>Import from the EAD Folder</strong>
             <div class="content_box">
-
-                <p id="convert_message">On this page you can convert EAD files or import EAC-CPF files that you have
-                    placed in the 'ead' folder during the install process.</p>
-
+                <p>Import EAD files that are located in the EAD folder</p>
                 <form action="ead_convert_class.php">
 
-                    <input type="hidden" name='dir' value="<?php echo $ead_path ?>"></input>
+                    <input type="hidden" name='dir' value="<?php echo $ead_path ?>" />
                     <button type="button" id="convertEad2Eac" name="convertEad2Eac"
-                            class="pure-button pure-button-primary">Import
+                            class="pure-button ramp-button">Import
                     </button>
                     <span id="file_estimator" style="display: none;"></span>
                     <div id="results"></div>
                 </form>
-                <p> After importing you can export and download the records.</p>
+
                 <div id="exporting"></div>
             </div>
-
-
         </div>
-        <div class="pure-u-1-2">
-            <h1>Upload an EAD</h1>
-            <form enctype="multipart/form-data" action="ajax/uploader.php" method="POST">
-                <input type="hidden" name="MAX_FILE_SIZE" value="30000000"/>
-                Upload an EAD: <input name="ead" type="file"/>
-                <input type="submit" value="Upload EAD"/>
-            </form>
 
-        </div>
 
 
     </div> <!-- end pure-g -->
+
+        </div>
 
 <?php include('footer.php'); ?>
