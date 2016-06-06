511565c10c4408abe17094744cdfc8bb95ff75e3
James Little <j.little@miami.edu>
2016-06-06 12:20:07 -0400

    Adding download link to all records page

--- all-items.php
+++ all-items.php
@@ -25,6 +25,8 @@ include('header.php');
           echo "<td>WorldCat Ingest Status</td>";
           echo "<td>Wiki Ingest Status</td>";
           echo "<td>Delete Record</td>";
+          echo "<td>Download Record</td>";
+
           echo "</tr>";
           foreach($allItems as $item):
 
@@ -35,6 +37,7 @@ include('header.php');
             echo "<td>".$item['ingest_status']['statuses'][1]['status']."</td>";
             echo "<td>".$item['ingest_status']['statuses'][2]['status']."</td>";
             echo "<td><a href='#' onclick='deleteRecord({$item['eac_id']})'><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></a></td>";
+            echo "<td><a href='ajax/download_eac.php?eac_id={$item['eac_id']}'><i class=\"fa fa-download\" aria-hidden=\"true\"></i></a></td>";
 
             echo "</tr>";
           endforeach;
