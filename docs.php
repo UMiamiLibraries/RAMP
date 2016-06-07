1d93778504f4b3c08e232a3895f3670b9f0a8655
pvillanueva01 <pvillanueva@libwet238.local>
2016-06-07 13:13:53 -0400

    Markup updates

--- docs.php
+++ docs.php
@@ -2,14 +2,26 @@
 include('header.php');
 ?>
 
-<div class="pure-g-r">
-  <div class="pure-u-1">
-    <div class="content_box">
-      <h1>Documentation</h1>
-      
-    </div>
-  </div>   
+<div class="recordtitle-bkg">    
+   <div class="recordtitle">
+        <div class="inner-area"><h2 class="page-top-heading">Documentation</h2></div>
+   </div>
 </div>
+
+<div class="decoration-bar">
+    <div class="pure-g">
+        <div class="pure-u-1-4 decor1"></div>
+        <div class="pure-u-1-4 decor2"></div>
+        <div class="pure-u-1-4 decor3"></div>
+        <div class="pure-u-1-4 decor4"></div>
+    </div>
+</div>
+
+<div class="inner-area">
+  <div class="content_box" id="docs_box">
+      
+  </div>
+</div>  
 
 
 <?php
