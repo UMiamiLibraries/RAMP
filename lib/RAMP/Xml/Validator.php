e5a67c4f7943591fd6ed068ddd8355bddcef605c
cbrownroberts <charlesbrownroberts@miami.edu>
2016-06-07 09:37:09 -0400

    added basic xml file validation functions to Validator.php. the existing function checks for valid eac, the new functions just check for basic xml validation with php built in xml functions.

--- lib/RAMP/Xml/Validator.php
+++ lib/RAMP/Xml/Validator.php
@@ -32,12 +32,49 @@ class Validator
         {
 
             // libxml_get_errors returns an array of the arrays
-
             $errors = json_encode(libxml_get_errors());
             echo $errors;
 
         }
 
     }
+    
+
+    /**
+     * @param string $xmlFilename Path to the XML file
+     * @param string $version 1.0
+     * @param string $encoding utf-8
+     * @return bool
+     */
+    public function isXMLFileValid($xmlFilename, $version = '1.0', $encoding = 'utf-8')
+    {
+        $xmlContent = file_get_contents($xmlFilename);
+        return $this->isXMLContentValid($xmlContent, $version, $encoding);
+    }
+
+    /**
+     * @param string $xmlContent A well-formed XML string
+     * @param string $version 1.0
+     * @param string $encoding utf-8
+     * @return bool
+     */
+    public function isXMLContentValid($xmlContent, $version = '1.0', $encoding = 'utf-8')
+    {
+        if (trim($xmlContent) == '') {
+            return false;
+        }
+
+        libxml_use_internal_errors(true);
+
+        $doc = new \DOMDocument($version, $encoding);
+        $doc->loadXML($xmlContent);
+
+        $errors = libxml_get_errors();
+        libxml_clear_errors();
+
+        return empty($errors);
+    }
+
+
 
 }
