731ddc6374689d31f7f1d7211840736c4e6d98de
pvillanueva01 <pvillanueva@libwet238.local>
2016-06-08 15:31:15 -0400

    Message for Install page

--- install.php
+++ install.php
@@ -1,24 +1,6 @@
 <?php include('header.php');
       include('installer/db_text.php');
       include('installer/write_db_text.php');
-use RAMP\Util\Database;
-try {
-    $db = Database::getInstance();
-
-     echo <<<EOT
-            
-            <div class="inner-area">
-              <div id="flash_message">
-                  <div class="success-message">
-                   <p>Successfully connected to MySQL</p>
-                  </div>
-              </div>
-            </div>
-EOT;
-
-} catch (mysqli_sql_exception $e) {
-
-}
 ?>
 
 <?php
@@ -31,4 +13,25 @@ if (isset($_POST['db_user'])) {
 ?>
 
 <?php include('installer/form.php'); ?>
+<?php 
+use RAMP\Util\Database;
+
+try {
+    $db = Database::getInstance();
+
+     echo <<<EOT
+            
+            <div class="inner-area">
+              <div id="flash_message">
+                  <div class="success-message">
+                   <p>Successfully connected to MySQL. <a href="index.php">Start using RAMP.</a></p>
+                  </div>
+              </div>
+            </div>
+EOT;
+
+} catch (mysqli_sql_exception $e) {
+
+}
+?>
 <?php include('footer.php') ?>
