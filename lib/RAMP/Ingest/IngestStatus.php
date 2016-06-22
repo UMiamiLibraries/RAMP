40d66c9a61a4308e4b3cbaffeec2a096258664cd
Jamie Little <j.little@miami.edu>
2016-06-22 09:56:47 -0400

    Update IngestStatus.php

--- lib/RAMP/Ingest/IngestStatus.php
+++ lib/RAMP/Ingest/IngestStatus.php
@@ -28,7 +28,7 @@ class IngestStatus
         $db = Database::getInstance();
         $mysqli = $db->getConnection();
 
-        $statement = $mysqli->prepare("SELECT eac_xml FROM ead_eac.eac WHERE eac_id = ?");
+        $statement = $mysqli->prepare("SELECT eac_xml FROM eac WHERE eac_id = ?");
         $statement->bind_param("s", $eac_id);
         $statement->execute();
         $statement->bind_result($result);
@@ -39,7 +39,7 @@ class IngestStatus
 
         $statement->close();
 
-        $wiki_stmt = $mysqli->prepare("SELECT wiki_text FROM ead_eac.mediawiki WHERE eac_id = ?");
+        $wiki_stmt = $mysqli->prepare("SELECT wiki_text FROM mediawiki WHERE eac_id = ?");
         $wiki_stmt->bind_param("s", $eac_id);
         $wiki_stmt->execute();
         $wiki_stmt->bind_result($result);
